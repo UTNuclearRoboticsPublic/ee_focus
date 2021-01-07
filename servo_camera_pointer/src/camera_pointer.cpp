4260fca3a6e0cd706b74b825aa6e8c864e021a83
AdamPettinger <adam.pettinger@utexas.edu>
2021-01-07 16:58:38 -0600

    Move main function to own file

--- servo_camera_pointer/src/camera_pointer.cpp
+++ servo_camera_pointer/src/camera_pointer.cpp
@@ -44,32 +44,6 @@
 #include <moveit_servo/status_codes.h>
 #include <thread>
 
-static const std::string LOGNAME = "camera_pointing";
-
-// Class for monitoring status of moveit_servo
-class StatusMonitor
-{
-public:
-  StatusMonitor(ros::NodeHandle& nh, const std::string& topic)
-  {
-    sub_ = nh.subscribe(topic, 1, &StatusMonitor::statusCB, this);
-  }
-
-private:
-  void statusCB(const std_msgs::Int8ConstPtr& msg)
-  {
-    moveit_servo::StatusCode latest_status = static_cast<moveit_servo::StatusCode>(msg->data);
-    if (latest_status != status_)
-    {
-      status_ = latest_status;
-      const auto& status_str = moveit_servo::SERVO_STATUS_CODE_MAP.at(status_);
-      ROS_INFO_STREAM_NAMED(LOGNAME, "Servo status: " << status_str);
-    }
-  }
-  moveit_servo::StatusCode status_ = moveit_servo::StatusCode::INVALID;
-  ros::Subscriber sub_;
-};
-
 namespace servo_camera_pointer
 {
 CameraPointer::CameraPointer(ros::NodeHandle& nh, std::unique_ptr<moveit_servo::PoseTracking> pose_tracking_object)
@@ -210,43 +184,3 @@ bool CameraPointer::stop()
   return true;
 }
 }  // namespace servo_camera_pointer
-
-/**
- * Instantiate the pose tracking interface.
- * Send a pose slightly different from the starting pose
- * Then keep updating the target pose a little bit
- */
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, LOGNAME);
-  ros::NodeHandle nh("~");
-  ros::AsyncSpinner spinner(8);
-  spinner.start();
-
-  // Load the planning scene monitor
-  planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor;
-  planning_scene_monitor = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
-  if (!planning_scene_monitor->getPlanningScene())
-  {
-    ROS_ERROR_STREAM_NAMED(LOGNAME, "Error in setting up the PlanningSceneMonitor.");
-    exit(EXIT_FAILURE);
-  }
-
-  planning_scene_monitor->startSceneMonitor();
-  planning_scene_monitor->startWorldGeometryMonitor(
-      planning_scene_monitor::PlanningSceneMonitor::DEFAULT_COLLISION_OBJECT_TOPIC,
-      planning_scene_monitor::PlanningSceneMonitor::DEFAULT_PLANNING_SCENE_WORLD_TOPIC,
-      false /* skip octomap monitor */);
-  planning_scene_monitor->startStateMonitor();
-
-  // Create the pose tracker
-  auto tracker = std::make_unique<moveit_servo::PoseTracking>(nh, planning_scene_monitor);
-
-  // Subscribe to servo status (and log it when it changes)
-  StatusMonitor status_monitor(nh, "status");
-
-  servo_camera_pointer::CameraPointer camera_pointer(nh, std::move(tracker));
-  camera_pointer.spin();
-
-  return EXIT_SUCCESS;
-}
