82b7627ea4a0ffc507df97e6e828878a60f39d4a
JackBorer <jackborer@utexas.edu>
2021-01-24 14:42:08 -0600

    First commit

---
+++ servo_camera_pointer/scripts/dynamic_tf.py
@@ -0,0 +1,104 @@
+#!/bin/python3
+
+import rospy
+import tf2_ros
+import geometry_msgs.msg
+
+import tty
+import sys
+import termios
+
+
+if __name__ == '__main__':
+    try:
+        camera_frame_name, target_frame_name = sys.argv[1:3]
+    except:
+        msg = """You did not provide command line frame args!
+        Example: 'rosrun servo_camera_pointer dynamic_tf.py ee_link demo_link'"""
+        rospy.logerr(msg)
+        quit()
+    
+    information_msg = """
+    ********************************************************************
+    dyanmic_tf.py 
+    ********************************************************************
+    Used to dynamically control a frame to demonstrate 
+    the servo_camera_pointer capability.
+    ********************************************************************
+    Press the following keys to control the translation of the 
+    target frame.
+
+    -x : 1
+    +x : 2
+    -y : 3
+    +y : 4
+    -z : 5
+    +z : 6
+
+    Use ESC to exit!
+    ********************************************************************
+    """
+    print(information_msg)
+    
+    # Initialize node/publisher
+    rospy.init_node('dynamic_tf_broadcaster')
+    broadcaster = tf2_ros.StaticTransformBroadcaster()
+    
+    # Initialize transform frame ids
+    transform_stamped = geometry_msgs.msg.TransformStamped()
+    transform_stamped.header.frame_id = camera_frame_name
+    transform_stamped.child_frame_id = target_frame_name
+    transform_stamped.transform.rotation.w = 1.0
+
+    # Publish first tf
+    broadcaster.sendTransform(transform_stamped)
+
+    state = {"x": 0.0, "y": 0.0, "z": 0.0}
+
+    # Store original terminal settings so we can restore the terminal when done
+    orig_settings = termios.tcgetattr(sys.stdin)
+
+    # Makes it so we don't need to hit enter for every input
+    tty.setcbreak(sys.stdin)
+
+    key = 0
+    increment = 0.1 # (m) How much each key stroke will translate in the given direction
+    while key != chr(27): # ESC
+        try:
+            key = sys.stdin.read(1)[0]
+        except:
+            msg = "An input error occured, restoring original terminal settings and exiting"
+            rospy.logerr(msg)
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)  
+            quit()            
+
+        if not key.isdigit():
+            print("You selected a letter or special character not a number!")
+        elif int(key) == 1:
+            state["x"] -= increment
+            print("x: " + str(state["x"]))
+        elif int(key) == 2:
+            state["x"] += increment
+            print("x: " + str(state["x"]))
+        elif int(key) == 3:
+            state["y"] -= increment
+            print("y: " + str(state["y"]))
+        elif int(key) == 4:
+            state["y"] += increment
+            print("y: " + str(state["y"]))
+        elif int(key) == 5:
+            state["z"] -= increment
+            print("z: " + str(state["z"]))
+        elif int(key) == 6:
+            state["z"] += increment
+            print("z: " + str(state["z"]))
+        else:
+            print("Not a valid number key!")
+
+        transform_stamped.transform.translation.x = state["x"]
+        transform_stamped.transform.translation.y = state["y"]
+        transform_stamped.transform.translation.z = state["z"]
+        broadcaster.sendTransform(transform_stamped)
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)  
+    
